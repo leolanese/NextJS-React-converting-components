ab915a2ab97fb726f00266159e8044043fda2471
leolanese <laneseleo@gmail.com>
2022-08-06 13:01:48 +0100

    updating nextjs components

--- pages/get/stateAndComponentDidMountAndComponentDidUpdate.js
+++ pages/get/stateAndComponentDidMountAndComponentDidUpdate.js
@@ -1,32 +1,43 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  // you did not have to write separate code for the componentDidMount() and
+  // this kind of optimise the code as:
+  // we didn't have to write separate code for the componentDidMount() and
   // componentDidUpdate() functions. With the useEffect() Hook, you get the
   // functionality of both. This is because useEffect() runs both after the 
-  // initial render and after every subsequent update by default.
+  // initial render and after every subsequent update by default
   const [header, setHeader] = useState('Leo');
 
   useEffect(() => {
-    const newheader = document.querySelectorAll('#header')[0];
-    setTimeout(() => {
-      newheader.innerHTML = header;
-    }, 2000);
+    console.log(header);
   });
 
+  const logName = () => {
+    console.log(header);
+  };
+
   const handleHeaderInput = e => {
-    setHeader(e.target.value);
+    setHeader( e.target.value );
   };
 
   return (
     <div>
-      <h1 id="header">Adding Hooks to a Class with State, componentDidMount, and componentDidUpdate</h1>
+      <h1 id="header">
+          Adding Hooks to a Class with State, componentDidMount, and componentDidUpdate
+      </h1>
 
       <input
         type="text"
         onChange={handleHeaderInput}
         value={header}
       />
+
+      <button 
+        onClick={logName} 
+        className="btn btn-large right">
+        click-me
+      </button>
+
     </div>
   );
 };
